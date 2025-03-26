22f3b4dfeadbf401ea6e81395bc6bf6dce3cce14
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-03-26 16:12:59 +0100

    update to topsongs from lastFM api

--- src/pages/TopSongs.jsx
+++ src/pages/TopSongs.jsx
@@ -2,14 +2,12 @@ import Footer from '../components/Footer';
 import NavHeader from '../components/NavHeader';
 import mockData from '../data/mockData.json';
 import lastFMKey from '../data/keys';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function TopSongs() {
-  // Samla alla låtar från alla användare i en enda array
-  const allSongs = mockData.users.flatMap((user) => user.topSongs);
+  const [musicImg, setMusicImg] = useState();
 
-  // Sortera låtarna från mest till minst streams
-  const sortedSongs = allSongs.sort((a, b) => b.streams - a.streams);
+  const [fetchTopSongs, setFetchTopSongs] = useState([]);
 
   useEffect(() => {
     async function getMusicArtist(artist) {
@@ -20,26 +18,57 @@ function TopSongs() {
 
         const data = await response.json();
         console.log('YOOO:', data);
+
+        setMusicImg(data.album.image[3]['#text']);
+      } catch (error) {
+        console.error('Error fetching:', error);
+      }
+    }
+
+    async function getTopSongs(artist) {
+      try {
+        const response = await fetch(
+          `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${lastFMKey}&format=json`
+        );
+
+        const data = await response.json();
+        console.log('YOOO 222:', data.tracks.track);
+
+        setFetchTopSongs(data.tracks.track);
+        console.log('yes', fetchTopSongs);
       } catch (error) {
         console.error('Error fetching:', error);
       }
     }
     getMusicArtist();
+    getTopSongs();
   }, []);
+
+  /* tracks.track[0] */
 
   return (
     <>
       <NavHeader />
       <div className='flex flex-col items-center py-12 px-8'>
-        <h2 className='text-4xl text-primary font-bold mb-8'>Top songs</h2>
+        <h2 className='text-4xl text-primary font-bold mb-8'>
+          Top Tracks Chart
+        </h2>
+        <div>
+          <img src={musicImg} alt='' />
+        </div>
+
         <ul className='list-decimal pl-5 text-text dark:text-text-dark'>
-          {sortedSongs.map((songObj, index) => (
+          {fetchTopSongs.map((songObj, index) => (
             <li
               key={index}
               className='py-2 border-b border-gray-200 dark:border-gray-800  '
             >
-              <span className='font-semibold'>{songObj.song}</span> -{' '}
-              {songObj.artist} ({songObj.streams} streams)
+              <p className=''>
+                <strong>{songObj.name}</strong> - {songObj.artist.name}
+              </p>
+              <p className='text-text-muted dark:text-text-muted-dark'>
+                Streams: {songObj.listeners}
+              </p>
             </li>
           ))}
         </ul>
